c5363d97c4e58f154e1928767f8f8f85b18f9d89
majidehamide <abd.majidehamide@gmail.com>
2016-03-09 01:57:50 +0700

    commit admin view

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,9 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
+Route::get('/admin', function () {
+    return view('admin/index');
+});
 /*
 |--------------------------------------------------------------------------
 | Application Routes
